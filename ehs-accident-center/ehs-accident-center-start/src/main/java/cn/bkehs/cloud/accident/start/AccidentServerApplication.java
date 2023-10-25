8645b1624bf9ab24d77a83501b81cdaa7128befd
xiaowenhui <xiaowenhui@ehsconsulting.com.cn>
2023-10-25 17:31:23 +0800

    add accident

---
+++ ehs-accident-center/ehs-accident-center-start/src/main/java/cn/bkehs/cloud/accident/start/AccidentServerApplication.java
@@ -0,0 +1,20 @@
+package cn.bkehs.cloud.accident.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目的启动类
+ *
+ * @author beckwell-xwh
+ */
+@SpringBootApplication
+public class AccidentServerApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(AccidentServerApplication.class, args);
+
+    }
+
+}
